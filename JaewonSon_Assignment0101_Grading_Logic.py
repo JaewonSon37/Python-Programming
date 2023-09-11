e9f0fbead03233652ec496aed49d7ae74a3f47d7
JaewonSon37 <wodnjs980307@naver.com>
2023-09-10 21:24:26 -0500

    first commit

---
+++ JaewonSon_Assignment0101_Grading_Logic.py
@@ -0,0 +1,51 @@
+## Name: Jaewon Son
+## Date: September 10 2023
+## Honor Statement: I have not given or received any unauthorized assistance on this assignment.
+## Link:
+
+
+def compute_grade():
+    """Function for compute total score of a assignment.
+
+    Returns:
+        int: Returns integer based on number typed by a prompt.
+    """
+
+    question1 = input('Did you submit the assignment as a single uncompressed .py file? [Yes or No]: ')
+
+    if question1 == 'No':
+        return('Your assignment grade is 0')
+
+    question2 = input('Did you include your name and date in the assignment? [Yes or No]: ')
+
+    if question2 == 'No':
+        return print('Your assignment grade is 0')
+
+    question3 = input('Did you include the honor statement in the assignment? [Yes or No]: ')
+
+    if question3 == 'No':
+        return('Your assignment grade is 0')
+
+    question4 = input('Did you include a link to an YouTube video in the assignment? [Yes or No]: ')
+
+    if question4 == 'No':
+        return('Your assignment grade is 0')
+
+    question5 = input('Evaluate the correctness of the code.\nEnter the point between 1 to 10: ')
+
+    question6 = input('Evaluate the elegance of the code.\nEnter the point between 1 to 10: ')
+
+    question7 = input('Evaluate the code hygiene.\nEnter the point between 1 to 10: ')
+
+    question8 = input('Evaluate the quality of the discussion on the video.\nEnter the point between 1 to 10: ')
+
+    question9 = input('Did you submit the assignment before the deadline? [Yes or No]: ')
+
+    if question9 == 'No':
+        question10 = input('How many hours late did you submit it?: ')
+        return eval(question5) + eval(question6) + eval(question7) + eval(question8) - 0.4 * eval(question10)
+    else:
+        return eval(question5) + eval(question6) + eval(question7) + eval(question8)
+
+total_score = compute_grade()
+print(total_score)
---
+++ JaewonSon_Assignment0102_Coprime.py
@@ -0,0 +1,46 @@
+## Name: Jaewon Son
+## Date: September 10 2023
+## Honor Statement: I have not given or received any unauthorized assistance on this assignment.
+## Link:
+
+
+def coprime(coprime_test_number1, coprime_test_number2): 
+  
+  """Function for checking whether two numbers are coprime or not.
+
+  Args:
+      coprime_test_number1 (int): Natural number typed by a prompt in the first order.
+      coprime_test_number2 (int): Natural number typed by a prompt in the second order.
+
+  Returns:
+      bool: Returns true when the numbers are coprime and returns false when the numbers are not coprime.
+  """
+
+  if coprime_test_number1 == 1 or coprime_test_number2 == 1:
+    return True
+  else:
+    count = 0
+    if coprime_test_number1 < coprime_test_number2:
+      for i in range(2, coprime_test_number1 + 1):
+        if coprime_test_number1 % i == 0:
+          if coprime_test_number2 % i == 0:
+            count += 1
+    else:
+      for i in range(2, coprime_test_number2 + 1):
+        if coprime_test_number2 % i == 0:
+          if coprime_test_number1 % i == 0:
+            count += 1
+
+    return count < 1
+
+while True:
+
+  coprime_test_number1 = input('Enter the First Number or Exit: ')
+  if coprime_test_number1 == 'Exit':
+    print('Thank you for using Coprime test!')
+    break
+  coprime_test_number2 = input('Enter the Second Number: ')
+  if coprime(eval(coprime_test_number1), eval(coprime_test_number2)):
+    print('Those two numbers are coprime.')
+  else:
+    print('Those two numbers are not coprime.')
