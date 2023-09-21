b64c071ebe37823f36162e993a5833a1a68b7315
Jaewon Son <144640914+JaewonSon37@users.noreply.github.com>
2023-09-21 14:30:12 -0500

    third commit

--- JaewonSon_Assignment_0202_Stem_and_Leaf_Implementation.py
+++ JaewonSon_Assignment_0202_Stem_and_Leaf_Implementation.py
@@ -1,7 +1,7 @@
 ## Name: Jaewon Son
 ## Date: September 20 2023
 ## Honor Statement: I have not given or received any unauthorized assistance on this assignment.
-## Link: 
+## Link: https://youtu.be/k9uXb7_Bt1A
 
 
 import os
@@ -15,9 +15,9 @@ def ask_directory_path():
         str: Return path of a directory if the 3 text files exist. Else return empty.
     """
     
-    dir_source = input("\nPlease provide a directory path that contains the 3 text files: ")
+    dir_source = input("\nPlease provide a directory path that contains the 3 text files: ")   # A path of directory entered by a user
     
-    if (os.path.isfile(os.path.join(dir_source, 'StemAndLeaf1.txt'))):
+    if (os.path.isfile(os.path.join(dir_source, 'StemAndLeaf1.txt'))):   # Checking StemAndLeaf().txt file exists in the directory
         if (os.path.isfile(os.path.join(dir_source, 'StemAndLeaf2.txt'))):
             if (os.path.isfile(os.path.join(dir_source, 'StemAndLeaf3.txt'))):
                 return dir_source
@@ -57,12 +57,12 @@ def read_text_file(filename):
     """Function to read data from a file and return it as a list.
 
     Returns:
-        list: Return a list function based on data from a text file.
+        list: Return a list function based on data from a text file. Else, return None.
     """
         
     try:
-        with open(filename, 'r') as file:
-                data = [int(line.strip()) for line in file.readlines()]
+        with open(filename, 'r') as file:   # Calling a text file
+                data = [int(line.strip()) for line in file.readlines()]    # Generating a list based on data in a text file
         return data
     
     except:
@@ -77,14 +77,14 @@ def stem_and_leaf_plot(data):
     
     stem_and_leaf_dict = {}
         
-    for number in data:
-        stem, leaf = divmod(number, 10)
-        if stem not in stem_and_leaf_dict:
+    for number in data:   # 
+        stem, leaf = divmod(number, 10)   # Separating numbers as stems and leaves
+        if stem not in stem_and_leaf_dict:   # Adding values in the dictionary
             stem_and_leaf_dict[stem] = []
         stem_and_leaf_dict[stem].append(leaf)
         
     for stem, leaves in sorted(stem_and_leaf_dict.items()):
-        print(f"{stem} | {' '.join(map(str, sorted(leaves)))}")
+        print(f"{stem} | {' '.join(map(str, sorted(leaves)))}")   # Generating a stem and leaf plot
 
 
 def exit_program():
@@ -101,7 +101,7 @@ def main_function():
     """
 
     dir_source = ask_directory_path()
-    if (len(dir_source) == 0):
+    if (len(dir_source) == 0):   # Terminating the program if a user enters an invalid path  
         os._exit(0)
 
     greet_to_user()
@@ -114,8 +114,8 @@ def main_function():
             break
 
         elif choice in ('1', '2', '3'):
-            data = read_text_file(dir_source + f"\StemAndLeaf{choice}.txt")
-            stem_and_leaf_plot(data)
+            data = read_text_file(os.path.join(dir_source, f"StemAndLeaf{choice}.txt"))   # Calling a text file and generating a list
+            stem_and_leaf_plot(data)   
 
         else:
             print("\nInvalid choice. Please enter 1, 2, 3, or 0 to exit.")
